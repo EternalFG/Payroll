6d55690cac1deb42c9ec2ca87cca3e04f3674ed7
EternalFG <72083320+EternalFG@users.noreply.github.com>
2022-11-08 21:49:02 +0200

    Create class EmployeeTable

---
+++ Payroll/Core/Data/EmployeeDB/EmployeeTable.cs
@@ -0,0 +1,41 @@
+﻿using LiteDB;
+using Payroll.Core.Models;
+using Payroll.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll.Core.Data.EmployeeDB
+{
+    public static class EmployeeTable
+    {
+        public static ILiteCollection<EmployeeBase> Employees;
+
+        static EmployeeTable()
+        {
+            Employees = Program.Database.GetCollection<EmployeeBase>("Employees");
+        }
+        
+        public static EmployeeBase FindEmployee(string employeeId)
+        {
+            return Employees.FindById(employeeId);
+        }
+
+        public static void AddEmployee(EmployeeBase employee)
+        {
+            Employees.Insert(employee);
+        }
+
+        public static bool UpdateEmployee(EmployeeBase employee)
+        {
+            return Employees.Update(employee);
+        }
+
+        public static bool DeleteEmployee(EmployeeBase employee)
+        {
+            return Employees.Delete(employee.Id);
+        }
+    }
+}
