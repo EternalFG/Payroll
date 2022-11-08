76f087488d3595e61454526689abc88ec4541bba
EternalFG <72083320+EternalFG@users.noreply.github.com>
2022-11-08 21:50:07 +0200

    Create form Authorization

---
+++ Payroll/Core/Forms/Authorization.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Payroll.Core.Forms
+{
+    public partial class Authorization : Form
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBoxShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxShowPassword.Checked)
+            {
+                textPassword.PasswordChar = '\0';
+            }
+            else
+            {
+                textPassword.PasswordChar = '•';
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            new Authentication().Show();
+            this.Hide();
+        }
+    }
+}
