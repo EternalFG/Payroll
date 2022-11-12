1524130da55b4ba30d8e6372d90c0ec9905c31f1
EternalFG <72083320+EternalFG@users.noreply.github.com>
2022-11-12 22:07:01 +0200

    Create class User

---
+++ Payroll/Core/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using LiteDB;
+using Payroll.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll.Core.Models
+{
+    public class User 
+    {
+        [BsonId] public string UserName { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
